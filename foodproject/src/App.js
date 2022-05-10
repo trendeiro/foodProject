2a4e82318d14a8e7ba6b247ae5d400e9b26465dd
trendeiro <torendeiro@gmail.com>
2022-05-10 17:47:19 +0100

    finished

--- foodproject/src/App.js
+++ foodproject/src/App.js
@@ -1,15 +1,28 @@
-import { Fragment } from "react";
+import { useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 
 function App() {
+  const [cartIsShown, setcartIsShown] = useState(false)
+
+  const showCartHandler = () => {
+    setcartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setcartIsShown(false);
+  }
+ 
   return (
-    <Fragment >
-      <Header/>
+    <CartProvider >
+      {cartIsShown && <Cart onClose={hideCartHandler}/>}
+      <Header onShowCart={showCartHandler}/>
       <main>
         <Meals/>
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
